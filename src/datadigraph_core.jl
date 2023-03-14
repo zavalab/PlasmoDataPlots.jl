d01057286a538e4c51f393a589d594c6f9fdfb5c
dlcole3 <dlcole3@wisc.edu>
2023-03-14 15:56:15 -0500

    Updated code for directed graphs

---
+++ src/datadigraph_core.jl
@@ -0,0 +1,65 @@
+function plot_graph(dg::DataGraphs.DataDiGraph;
+    get_new_positions::Bool=false,
+    dag_positions::Bool=false,
+    layout = NetworkLayout.Spring(),
+    arrow_shift = .8,
+    node_size = 10,
+    node_color = :black,
+    edge_color = :gray,
+    edge_width = 2,
+    nlabels_fontsize = 12,
+    nlabels = nothing,
+    save_fig::Bool = false,
+    fig_name::String = "plot.png",
+)
+
+    if dag_positions
+        xs, ys, paths = LayeredLayouts.solve_positions(LayeredLayouts.Zarate(), dg.g)
+
+        add_node_dataset!(dg, xs, "x_positions")
+        add_node_dataset!(dg, ys, "y_positions")
+    end
+
+    if get_new_positions || !("x_positions" in dg.node_data.attributes) || !("y_positions" in dg.node_data.attributes)
+        if !dag_positions
+            am = DataGraphs.adjacency_matrix(dg)
+            pos = layout(am)
+
+            for i in 1:length(pos)
+                add_node_data!(dg, dg.nodes[i], pos[i][1], "x_positions")
+                add_node_data!(dg, dg.nodes[i], pos[i][2], "y_positions")
+            end
+        end
+    end
+
+    function layout_graph(g)
+        node_vec = Vector{GeometryBasics.Point}()
+        for i in 1:length(dg.nodes)
+            push!(node_vec, Point(get_node_data(dg, dg.nodes[i], "x_positions"), get_node_data(dg, dg.nodes[i], "y_positions")))
+        end
+
+        return node_vec
+    end
+
+    layout_fn = layout_graph
+
+    plt = GraphMakie.graphplot(
+        dg.g,
+        layout = layout_fn,
+        arrow_shift = arrow_shift,
+        node_size = node_size,
+        node_color = node_color,
+        edge_color = edge_color,
+        edge_width = edge_width,
+        nlabels_fontsize = nlabels_fontsize,
+        nlabels = nlabels
+    )
+
+    Makie.hidespines!(plt.axis)
+    Makie.hidedecorations!(plt.axis)
+    if save_fig
+        Makie.save(fig_name, plt)
+    end
+
+    return plt
+end
---
+++ src/datagraph_core.jl
@@ -0,0 +1,202 @@
+
+function plot_graph(dg::DataGraphs.DataGraph;
+    get_new_positions::Bool=false,
+    plot_edges::Bool=true,
+    C::Real=1.0,
+    K::Real=.1,
+    iterations::Int=300,
+    tol::Real=.1,
+    xdim = 800,
+    ydim = 800,
+    linewidth = 1,
+    linealpha = 1,
+    linecolor = :gray,
+    line_z = nothing,
+    line_z_grad = :inferno,
+    nodecolor = :black,
+    nodesize = 5,
+    nodestrokewidth = 1,
+    nodestrokecolor = :black,
+    nodestrokealpha = :none,
+    node_z = nothing,
+    framestyle=:box,
+    legend::Bool = false,
+    save_fig::Bool = false,
+    fig_name::String = "plot.png",
+)
+
+    am = DataGraphs.adjacency_matrix(dg)
+    nodes = dg.nodes
+
+    if get_new_positions || !("x_positions" in dg.node_data.attributes) || !("y_positions" in dg.node_data.attributes)
+        pos = NetworkLayout.sfdp(Graphs.SimpleGraph(am); tol = tol, C = C, K = K, iterations = iterations)
+        if !("x_positions" in dg.node_data.attributes)
+            add_node_attribute!(dg, "x_positions", 0.0)
+        end
+
+        if !("y_positions" in dg.node_data.attributes)
+            add_node_attribute!(dg, "y_positions", 0.0)
+        end
+
+        for i in 1:length(pos)
+            add_node_data!(dg, dg.nodes[i], pos[i][1], "x_positions")
+            add_node_data!(dg, dg.nodes[i], pos[i][2], "y_positions")
+
+        end
+    end
+
+    if line_z != nothing
+        if length(line_z) != length(dg.edges)
+            error("Length of line_z argument is different than the number of edges")
+        end
+
+        line_z_cgrad = cgrad(line_z_grad)
+        edge_min = minimum(line_z)
+        edge_max = maximum(line_z)
+        edge_span = edge_max - edge_min
+    end
+
+
+    plt = Plots.plot(framestyle = framestyle, grid = false, size = (xdim, ydim), axis = nothing, legend = legend)
+    if plot_edges
+        if line_z != nothing
+            Plots.plot!([],[], line_z = line_z, linecolor = line_z_cgrad, label = :none)
+            for (j, i) in enumerate(dg.edges)
+                from = i[1]
+                to   = i[2]
+
+                if line_z != nothing
+                    edge_val = (line_z[j] - edge_min) / edge_span
+                    color = line_z_cgrad[Int(floor(edge_val * 255 + 1))]
+
+                    #line_options = Dict(:linewidth => linewidth, :linealpha => linealpha, line_z => color)
+                    line_options = Dict(:linecolor => color, :linewidth => linewidth, :linealpha => linealpha)
+                else
+                    line_options = Dict(:linecolor => linecolor, :linewidth => linewidth, :linealpha => linealpha)
+
+                end
+
+                x_from = get_node_data(dg, from, "x_positions")
+                x_to   = get_node_data(dg, to, "x_positions")
+                y_from = get_node_data(dg, from, "y_positions")
+                y_to   = get_node_data(dg, to, "y_positions")
+
+                Plots.plot!(plt,[x_from, x_to], [y_from, y_to];
+                    label=:none,
+                    line_options...
+                )
+            end
+        else
+            x_points = Vector{Vector}()
+            y_points = Vector{Vector}()
+            for i in dg.edges
+                x_from = get_node_data(dg, nodes[i[1]], "x_positions")
+                x_to   = get_node_data(dg, nodes[i[2]], "x_positions")
+                y_from = get_node_data(dg, nodes[i[1]], "y_positions")
+                y_to   = get_node_data(dg, nodes[i[2]], "y_positions")
+
+                push!(x_points, [x_from, x_to])
+                push!(y_points, [y_from, y_to])
+            end
+
+            #line_options = Dict(:linewidth => linewidth, :linealpha => linealpha, :linecolor => linecolor, :label => :none)
+            if typeof(linecolor) <: Vector && length(linecolor) == length(dg.edges)
+                for i in 1:length(linecolor)
+                    Plots.plot!(plt, x_points[i], y_points[i], linecolor = linecolor[i], linewidth = linewidth, linealpha = linealpha, label = :none)
+                end
+            else
+            Plots.plot!(plt, x_points, y_points, linecolor = linecolor, linewidth = linewidth, linealpha = linealpha, label = :none)
+            end
+        end
+    end
+
+    attribute_map = dg.node_data.attribute_map
+
+    Plots.scatter!(plt, get_node_data(dg)[:, attribute_map["x_positions"]], get_node_data(dg)[:, attribute_map["y_positions"]];
+        markercolor=nodecolor,
+        markersize=nodesize,
+        markerstrokewidth = nodestrokewidth,
+        markerstrokecolor = nodestrokecolor,
+        markerstrokealpha = nodestrokealpha,
+        marker_z = node_z,
+        label=:none,
+    )
+
+    if save_fig
+        Plots.savefig(fig_name)
+    end
+
+    display(plt)
+    return plt
+end
+
+# Fix to set in place
+function set_matrix_node_positions!(dg::DataGraphs.DataGraph, mat)
+    dim1, dim2 = size(mat)
+
+    node_x_vals = zeros(length(dg.nodes))
+    node_y_vals = zeros(length(dg.nodes))
+    for i in 1:length(dg.nodes)
+        node_val  = dg.nodes[i]
+        node_x    = Float64(node_val[2] * 5)
+        node_y    = Float64((dim1 - node_val[1] * 5))
+
+        node_x_vals[i] = node_x
+        node_y_vals[i] = node_y
+
+    end
+
+    add_node_dataset!(dg, node_x_vals, "x_positions")
+    add_node_dataset!(dg, node_y_vals, "y_positions")
+end
+
+function set_tensor_node_positions!(dg::DataGraphs.DataGraph, tensor)
+    dim1, dim2, dim3 = size(tensor)
+
+    function get_es(a0, b0, c0, b1, c1, c2)
+        a1 = (-b0 * b1 - c0 * c1) / a0
+        b2 = (a0 * c1 * c2 / a1 - c0 * c2) / (b0 - a0 * b1 / a1)
+        a2 = (-b1 * b2 - c1 * c2) / a1
+
+        return [a1, b1, c1], [a2, b2, c2]
+    end
+
+    n  = [2, 5, -1.5]
+    b1 = 3
+    c1 = .2
+    c2 = 1
+    e1, e2 = get_es(n[1], n[2], n[3], b1, c1, c2)
+    e1_norm = (e1[1]^2 + e1[2]^2 + e1[3]^2)^(.5)
+    e2_norm = (e2[1]^2 + e2[2]^2 + e2[3]^2)^(.5)
+
+    node_x_vals = zeros(length(dg.nodes))
+    node_y_vals = zeros(length(dg.nodes))
+    for i in 1:length(dg.nodes)
+        node_val = dg.nodes[i]
+        node_x = Float64(e1[1] * node_val[1] + e1[2] * node_val[2] + e1[3] * node_val[3]) / e1_norm
+        node_y = Float64(e2[1] * node_val[1] + e2[2] * node_val[2] + e2[3] * node_val[3]) / e2_norm
+
+        node_x_vals[i] = node_x
+        node_y_vals[i] = node_y
+    end
+
+    add_node_dataset!(dg, node_x_vals, "x_positions")
+    add_node_dataset!(dg, node_y_vals, "y_positions")
+end
+
+function set_node_positions!(
+    dg::D,
+    C::Real = 1.0,
+    K::Real = 0.1,
+    iterations::Int = 300,
+    tol::Real = 0.1
+) where {D <: Union{DataGraphs.DataGraph, DataGraphs.DataDiGraph}}
+    am = DataGraphs.adjacency_matrix(dg)
+
+    pos = NetworkLayout.sfdp(Graphs.SimpleGraph(am); tol = tol, C = C, K = K, iterations = iterations)
+    nodes = dg.nodes
+    for i in 1:length(pos)
+        add_node_data!(dg, nodes[i], pos[i][1], "x_positions")
+        add_node_data!(dg, nodes[i], pos[i][2], "y_positions")
+    end
+end
